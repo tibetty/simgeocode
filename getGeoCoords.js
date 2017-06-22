9977493ea0ae8322e62ac1cc5053d984117c7f1f
git <xihua.duan@tourank.com>
2017-06-22 11:23:12 +0800

    npm configuration changed

---
+++ getGeoCoords.js
@@ -0,0 +1,18 @@
+﻿#!/usr/bin/env node
+'use strict';
+
+const simGeocode = require('./simgeocode.js'),
+	path = require('path');
+	
+if (process.argv.length >= 3) {
+	simGeocode(process.argv[2], 'en')
+	.then(result => {
+		console.log(result.address);
+		console.log([result.coordinate[1],result.coordinate[0]]);
+		console.log(result.coordinate);
+	}).catch(err => {
+		console.error(err.toString());
+	});
+} else {
+	console.log('Usage: %s %s <name to geocode>', path.basename(process.argv[0]), path.basename(process.argv[1]));
+}
